5b70d38970dd8ef7de307d4c8b88dde3c2b65eb6
Toghrul <88154990+HashimovToghrul@users.noreply.github.com>
2026-02-01 14:18:33 +0400

    List

---
+++ SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by Togrul Hashimov on 01.02.26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
